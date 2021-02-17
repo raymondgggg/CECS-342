dfd99e6b4899c17bf556ec154dd6aebdeb40bafb
Raymond <raymond13gggg@gmail.com>
2021-02-16 18:31:17 -0800

    f#

--- Lab4/ListOps.fs
+++ Lab4/ListOps.fs
@@ -18,26 +18,63 @@ type Account =
 // count x coll
 //
 // count the number of values equal to x in coll.
-let rec count x coll = 
-    let mutable instances = 0
+let rec count x coll =
     match coll with 
-    | head :: tail when head = x -> 
-        instances <- instances + 1
-        count x tail
-    | _ -> 0
-
-
+    | [] -> 0
+    | head::tail when head = x -> 1 + (count x tail)
+    | _ :: tail -> count x tail
+    
 // countEvens coll
 //
 // count the number of even integers in coll.
-
+let rec countEvens coll =
+    match coll with 
+    | [] -> 0
+    | head::tail when head % 2 = 0 -> 1 + (countEvens tail)
+    | _ :: tail -> countEvens tail
 
 // lastElement coll
 //
 // return the last element in the list
-
+let rec lastElement coll =
+    let listLen = List.length coll
+    if listLen = 1 then
+        List.head coll
+    else
+        coll
+        |> List.tail
+        |> lastElement 
 
 // maxOverdrawn coll
 //
 // given a list of Accounts, return the largest Overdrawn amount, or 0 if none
 // are overdrawn
+
+let maxOverdrawn coll = 
+    let rec maxOverdrawnInner coll oAcctList =
+        match coll with 
+        | [] -> oAcctList
+        | head::tail -> 
+            match head with 
+            | Balance b -> maxOverdrawnInner tail oAcctList
+            | Empty -> maxOverdrawnInner tail oAcctList
+            | Overdrawn o -> 
+                let newOAcctList = List.append oAcctList [o]
+                maxOverdrawnInner tail newOAcctList
+    let x = maxOverdrawnInner coll [0]
+    List.max x
+
+
+           
+           
+
+            
+            
+           
+                
+
+       
+            
+        
+            
+                
